80256886513e3aac0325f08ce2c0178d7806a620
Владимир <svr93@i.ua>
2015-02-27 20:43:22 +0400

    databases.js_1.0

--- applications/main/config/databases.js
+++ applications/main/config/databases.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     pgsqlConnection: {
-      url: 'postgres://mil_svr93:39rvs_lim@localhost:5432/military', // connection string (required)
-      slowTime: 1000,                                    // time to log query as slow (optional, default: '2s', in milliseconds or string like '5s')
+      url: 'postgres://mil_svr93:39rvs_lim@localhost:5432/mil_svr93',
+      slowTime: 1000
     }
 };
